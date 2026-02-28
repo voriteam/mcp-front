744fcfaf99997278ccdcd6c5b09d1f067ca430c0
Clinton Blackburn <clintonb@vori.com>
2026-02-27 21:34:04 -0800

    Properly deployed MCP Toolbox for Databases

--- vori/run.sh
+++ vori/run.sh
@@ -1,7 +1,6 @@
 docker container rm -f mcp-front
 docker run -p 8080:8080 \
   --env-file .env \
-  -v $(pwd)/config.json:/app/config.json \
   -v "$HOME/.config/gcloud/application_default_credentials.json:/tmp/adc.json" \
   -e GOOGLE_APPLICATION_CREDENTIALS=/tmp/adc.json \
   --name mcp-front \
